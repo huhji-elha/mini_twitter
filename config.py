bd3eced4c4d7e461639f94dec8351eb4d122c0fd
huhji <huhji.elha@gmail.com>
2020-09-10 17:36:48 +0900

    add config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+
+
+from sqlalchemy import create_engine, text
+
+db = {
+        'user' : 'root',
+        'password' : 'test1234',
+        'host' : 'localhost',
+        'port' : 0000,
+        'database' : 'mini_twitter'
+    }
+
+db_url = f"mysql+mysqlconnector://{username}:{password}@{host}:{port}/{database}?charset=utf8"
+db = create_engine(db_url, encoding='utf-8', max_overflow = 0)
+
+
